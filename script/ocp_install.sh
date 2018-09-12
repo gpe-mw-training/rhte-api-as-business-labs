3c4d2a92bf5e19eaba6dd766f3f0fb03fae68235
Satya Jayanti <sjayanti@redhat.com>
2018-09-12 18:50:56 +0100

    Fixed script for installation.

--- script/ocp_install.sh
+++ script/ocp_install.sh
@@ -9,7 +9,7 @@ sleep 10s
 
 REGION=40ac
 OCP_DOMAIN=rhte.opentlc.com
-OCP_SUFFIX=apps.$OCP_DOMAIN
+OCP_SUFFIX=apps.$REGION.$OCP_DOMAIN
 
 # Start and End tenants.
 START_TENANT=1
@@ -46,6 +46,7 @@ END_TENANT=3
 ### IMPORT IMAGE STREAMS
 
 # Red Hat Decision Manager
+
 oc create -f https://raw.githubusercontent.com/jboss-container-images/rhdm-7-openshift-image/7.0.x/rhdm70-image-streams.yaml -n openshift
 
 ## Other necessary image streamsi (already installed):
@@ -60,21 +61,48 @@ oc create -f https://raw.githubusercontent.com/jboss-container-images/rhdm-7-ope
 ### IMPORT TEMPLATES
 
 # Red Hat SSO 7.2
+
+oc delete template sso72-x509-mysql-persistent -n openshift
+
+sleep 5s;
+
 oc create -f https://raw.githubusercontent.com/gpe-mw-training/rhte-api-as-business-labs/master/templates/sso72-x509-mysql-persistent.json -n openshift
 
 # Apicurio Studio
+
+oc delete template apicurio-studio -n openshift
+
+sleep 5s;
+
 oc create -f https://raw.githubusercontent.com/gpe-mw-training/rhte-api-as-business-labs/master/templates/apicurio-template.yml -n openshift
 
 # Microcks
+oc delete template microcks-persistent-no-keycloak -n openshift
+
+sleep 5s;
+
 oc create -f https://raw.githubusercontent.com/gpe-mw-training/rhte-api-as-business-labs/master/templates/microcks-persistent-no-keycloak-template.yml -n openshift
 
 # Red Hat Decision Manager S2I Template
+
+oc delete template rhdm70-kieserver-basic-s2i -n openshift
+
+sleep 5s;
+
 oc create -f https://raw.githubusercontent.com/gpe-mw-training/rhte-api-as-business-labs/master/templates/rhdm70-kieserver-basic-s2i.yaml -n openshift
 
 #Red Hat SSO 7.2 Ephimeral
-oc create -f https://raw.githubusercontent.com/pszuster/3ScaleTD/master/templates/sso72-x509-https.json
+oc delete template sso72-x509-https -n openshift
+
+sleep 5s;
+
+oc create -f  https://raw.githubusercontent.com/pszuster/3ScaleTD/master/templates/sso72-x509-https.json -n openshift
 
 # NodeJS S2I template
+oc delete template quoting-app -n openshift
+
+sleep 5s;
+
 oc create -f https://raw.githubusercontent.com/gpe-mw-training/rhte-api-as-business-labs/master/templates/nodejs-quoting-app-template.json -n openshift
 
 ### Create Lab Infra project
@@ -85,20 +113,21 @@ oc adm new-project lab-infra --admin=opentlc-mgr --description="Lab Infrastructu
 
 oc project lab-infra
 
-oc new-app --template=apicurio-studio --param=AUTH_ROUTE=http://$HOSTNAME_HTTP/auth --param=UI_ROUTE=$APICURIO_UI_ROUTE --param=API_ROUTE=$APICURIO_API_ROUTE --param=WS_ROUTE=$APICURIO_WS_ROUTE --param=API_JVM_MAX=2000m --param=API_MEM_LIMIT=3000Mi --param=WS_JVM_MAX=2000m --param=WS_MEM_LIMIT=2500Mi --param=UI_JVM_MAX=1800m --param=UI_MEM_LIMIT=2500Mi
+oc new-app --template=apicurio-studio --param=AUTH_ROUTE=http://$SSO_HOSTNAME_HTTP/auth --param=UI_ROUTE=$APICURIO_UI_ROUTE --param=API_ROUTE=$APICURIO_API_ROUTE --param=WS_ROUTE=$APICURIO_WS_ROUTE --param=API_JVM_MAX=2000m --param=API_MEM_LIMIT=3000Mi --param=WS_JVM_MAX=2000m --param=WS_MEM_LIMIT=2500Mi --param=UI_JVM_MAX=1800m --param=UI_MEM_LIMIT=2500Mi
 
 sleep 60s;
 
 
 ## RH SSO
 oc create serviceaccount sso-service-account
-oc policy add-role-to-user view system:serviceaccount:$(oc project -q):sso-service-account
-oc new-app --template=sso72-x509-mysql-persistent --param=SSO_ADMIN_USERNAME=$SSO_ADMIN_USERNAME --param=SSO_ADMIN_PASSWORD=$SSO_ADMIN_PASSWORD  --param=HOSTNAME_HTTP=$SSO_HOSTNAME_HTTP -param=HOSTNAME_HTTPS=$SSO_HOSTNAME_HTTPS
+
+oc policy add-role-to-user view system:serviceaccount:lab-infra:sso-service-account
+oc new-app --template=sso72-x509-mysql-persistent --param=SSO_ADMIN_USERNAME=$SSO_ADMIN_USERNAME --param=SSO_ADMIN_PASSWORD=$SSO_ADMIN_PASSWORD  --param=HOSTNAME_HTTP=$SSO_HOSTNAME_HTTP --param=HOSTNAME_HTTPS=$SSO_HOSTNAME_HTTPS
 
 sleep 60s;
 
 ##  RH SSO Realms & OAuthClient
-oc process -f https://raw.githubusercontent.com/gpe-mw-training/rhte-api-as-business-labs/master/templates/sso-oauth-realm-templates.yml--param=OPENSHIFT_MASTER=$OPENSHIFT_MASTER --param=KEYCLOAK_ROUTE_HOSTNAME=$KEYCLOAK_ROUTE_HOSTNAME --param=MICROCKS_ROUTE_HOSTNAME=$MICROCKS_ROUTE_HOSTNAME --param=APICURIO_UI_ROUTE_HOSTNAME=$APICURIO_UI_ROUTE_HOSTNAME --param=OPENSHIFT_OAUTH_CLIENT_NAME=$OPENSHIFT_OAUTH_CLIENT_NAME  | oc create -f -
+oc process -f https://raw.githubusercontent.com/gpe-mw-training/rhte-api-as-business-labs/master/templates/sso-oauth-realm-templates.yml  --param=OPENSHIFT_MASTER=$OPENSHIFT_MASTER --param=KEYCLOAK_ROUTE_HOSTNAME=$KEYCLOAK_ROUTE_HOSTNAME --param=MICROCKS_ROUTE_HOSTNAME=$MICROCKS_ROUTE_HOSTNAME --param=APICURIO_UI_ROUTE_HOSTNAME=https://$APICURIO_UI_ROUTE --param=OPENSHIFT_OAUTH_CLIENT_NAME=$OPENSHIFT_OAUTH_CLIENT_NAME | oc create -f - 
 
 sleep 30s;
 
@@ -124,14 +153,16 @@ sleep 60s;
 # Each user is given admin rights to their corresponding projects.
 
 
-    for i in $(seq ${startTenant} ${endTenant}) ; do
+    for i in $(seq $START_TENANT $END_TENANT) ; do
 
 	   
-	tenantId = user$i;
+	tenantId=user$i;
 
-        # Give users view access to the infra projects lab_infra & rhdm
+	echo "Now starting deployment for user :" $tenantId;
 
-	oc adm policy add-role-to-user view $tenantId -n lab_infra
+        # Give users view access to the infra projects lab-infra & rhdm
+
+	oc adm policy add-role-to-user view $tenantId -n lab-infra
 	oc adm policy add-role-to-user view $tenantId -n rhdm
 
 	# Create project for user sso (ephemeral)
@@ -143,9 +174,11 @@ sleep 60s;
 	# Install SSO (ephemeral)
 
 	oc project $tenantId-sso
+	sleep 5s;
 	oc create serviceaccount sso-service-account
 	oc policy add-role-to-user view system:serviceaccount:$tenantId-sso:sso-service-account
-	oc new-app --template=sso72-x509-https --param HOSTNAME_HTTP=$tenantId-sso-unsecured.$OCP_SUFFIX --param HOSTNAME_HTTPS=$tenantId-sso.$OC_SUFFIX --param SSO_ADMIN_USERNAME=admin --param SSO_ADMIN_PASSWORD=password --param SSO_SERVICE_USERNAME=admin --param SSO_SERVICE_PASSWORD=password --param SSO_REALM=3scaleRealm
+
+	oc new-app --template=sso72-x509-https --param HOSTNAME_HTTP=$tenantId-sso-unsecured.$OCP_SUFFIX --param HOSTNAME_HTTPS=$tenantId-sso.$OCP_SUFFIX --param SSO_ADMIN_USERNAME=admin --param SSO_ADMIN_PASSWORD=password --param SSO_SERVICE_USERNAME=admin --param SSO_SERVICE_PASSWORD=password --param SSO_REALM=3scaleRealm
 
 	sleep 60s;
 	# Create project for Syndesis
