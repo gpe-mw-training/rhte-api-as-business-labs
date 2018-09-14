0f3ac000121897c93a773cebb88dfee9f5446add
Satya Jayanti <sjayanti@redhat.com>
2018-09-14 14:55:41 +0100

    added POST method to cors.

--- clientapp/WebApp/server.js
+++ clientapp/WebApp/server.js
@@ -58,7 +58,7 @@ function startServer(){
 		res.locals.envVars = envVars;
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.setHeader('Access-Control-Request-Method', '*');
-		res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+		res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
 		res.setHeader('Access-Control-Allow-Headers', '*');
 		res.render('index.html');
 
