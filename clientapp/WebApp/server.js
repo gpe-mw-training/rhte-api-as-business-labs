5a9509b7dbb0f09ad2a9abd2b86b42fc75dcc99d
Satya Jayanti <sjayanti@redhat.com>
2018-09-14 15:13:45 +0100

    added Access-Control-Allow-Credentials:true for cors.

--- clientapp/WebApp/server.js
+++ clientapp/WebApp/server.js
@@ -57,6 +57,7 @@ function startServer(){
 		req.
 		res.locals.envVars = envVars;
 		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Credentials', true);
 		res.setHeader('Access-Control-Request-Method', '*');
 		res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
 		res.setHeader('Access-Control-Allow-Headers', '*');
